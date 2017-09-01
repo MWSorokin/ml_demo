df1fda069a71e2030eeaf3f3563ee0806a888786
msorokin <mikhail.sorokin@sap.com>
2017-09-01 13:14:10 +0300

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scikit-learn + numpy on Heroku Demo App
+
+Deploy with the https://devcenter.heroku.com/articles/container-registry-and-runtime
---
+++ webapp/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from sklearn import datasets, svm
+from numpy import arange
+
+app = Flask(__name__)
+
+# Load Dataset from scikit-learn.
+digits = datasets.load_digits()
+
+@app.route('/')
+def hello():
+    clf = svm.SVC(gamma=0.001, C=100.)
+    clf.fit(digits.data[:-1], digits.target[:-1])
+    prediction = clf.predict(digits.data[-1:])
+    size = arange(7).size
+
+    return jsonify({'prediction': repr(prediction), 'size': size})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
